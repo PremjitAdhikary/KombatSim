9a7f8c67624880904b048bbfd3b4597cc2f4f76a
Premjit Adhikary <adhikary.premjit@gmail.com>
2019-09-01 21:50:48 +0530

    Another Try

---
+++ precache-manifest.73157ceda219cf431d7a49a933f9cc94.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411f06dfc5d14b7be7e931018b647e63",
+    "url": "/KombatSim/index.html"
+  },
+  {
+    "revision": "d38b02e09f7dbaad0030",
+    "url": "/KombatSim/static/css/10.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "2ee93b58f454a48f11fb",
+    "url": "/KombatSim/static/css/11.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "06dbd72d0930689b8bec",
+    "url": "/KombatSim/static/css/12.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "bc67296e92270bbe09df",
+    "url": "/KombatSim/static/css/13.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "dc47cf150d6fa145c888",
+    "url": "/KombatSim/static/css/14.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "d8e962ff53674038ba0d",
+    "url": "/KombatSim/static/css/15.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "c81de632e99a5ad20c02",
+    "url": "/KombatSim/static/css/16.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "d6892c7bf426a67cc1df",
+    "url": "/KombatSim/static/css/17.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "a2e8ac96478e019cca94",
+    "url": "/KombatSim/static/css/18.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "af00b65a9d2057ccfd70",
+    "url": "/KombatSim/static/css/19.a23a2ad4.chunk.css"
+  },
+  {
+    "revision": "7d0b415f11f031c9ec9f",
+    "url": "/KombatSim/static/css/20.85f453ba.chunk.css"
+  },
+  {
+    "revision": "725d8a881dba308c3f57",
+    "url": "/KombatSim/static/css/21.85f453ba.chunk.css"
+  },
+  {
+    "revision": "a7a6855b601756763129",
+    "url": "/KombatSim/static/css/22.85f453ba.chunk.css"
+  },
+  {
+    "revision": "3a7861d07a4232270b0f",
+    "url": "/KombatSim/static/css/23.454797b3.chunk.css"
+  },
+  {
+    "revision": "a2f75f195be798537b1d",
+    "url": "/KombatSim/static/css/24.454797b3.chunk.css"
+  },
+  {
+    "revision": "04fa7b782cda051c422b",
+    "url": "/KombatSim/static/css/25.454797b3.chunk.css"
+  },
+  {
+    "revision": "1b375ac2d03b168e2bed",
+    "url": "/KombatSim/static/css/26.454797b3.chunk.css"
+  },
+  {
+    "revision": "c5aa0f68f684ddab6b5b",
+    "url": "/KombatSim/static/css/27.454797b3.chunk.css"
+  },
+  {
+    "revision": "0f489165629d9915cf8e",
+    "url": "/KombatSim/static/css/28.454797b3.chunk.css"
+  },
+  {
+    "revision": "1f447530fd8ac723028f",
+    "url": "/KombatSim/static/css/29.454797b3.chunk.css"
+  },
+  {
+    "revision": "8741718058f25ef778a2",
+    "url": "/KombatSim/static/css/3.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "5d2ccb3b3fbc7c2e289e",
+    "url": "/KombatSim/static/css/4.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "dd919a0f41ade560b771",
+    "url": "/KombatSim/static/css/5.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "1d8ff3776c3d6aba5bd7",
+    "url": "/KombatSim/static/css/6.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "019f4d5e3314cd0bf60e",
+    "url": "/KombatSim/static/css/7.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "53cbd40f4317614589fe",
+    "url": "/KombatSim/static/css/8.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "dcccc81c69bba25f019f",
+    "url": "/KombatSim/static/css/9.35a4e40f.chunk.css"
+  },
+  {
+    "revision": "899427cf660178b41f0e",
+    "url": "/KombatSim/static/css/main.b9f8eda0.chunk.css"
+  },
+  {
+    "revision": "d38b02e09f7dbaad0030",
+    "url": "/KombatSim/static/js/10.b5994fd8.chunk.js"
+  },
+  {
+    "revision": "2ee93b58f454a48f11fb",
+    "url": "/KombatSim/static/js/11.ae57fcfa.chunk.js"
+  },
+  {
+    "revision": "06dbd72d0930689b8bec",
+    "url": "/KombatSim/static/js/12.9d336899.chunk.js"
+  },
+  {
+    "revision": "bc67296e92270bbe09df",
+    "url": "/KombatSim/static/js/13.57faff90.chunk.js"
+  },
+  {
+    "revision": "dc47cf150d6fa145c888",
+    "url": "/KombatSim/static/js/14.b629bc3b.chunk.js"
+  },
+  {
+    "revision": "d8e962ff53674038ba0d",
+    "url": "/KombatSim/static/js/15.40af854a.chunk.js"
+  },
+  {
+    "revision": "c81de632e99a5ad20c02",
+    "url": "/KombatSim/static/js/16.68433c30.chunk.js"
+  },
+  {
+    "revision": "d6892c7bf426a67cc1df",
+    "url": "/KombatSim/static/js/17.cb732adb.chunk.js"
+  },
+  {
+    "revision": "a2e8ac96478e019cca94",
+    "url": "/KombatSim/static/js/18.36a44dc8.chunk.js"
+  },
+  {
+    "revision": "af00b65a9d2057ccfd70",
+    "url": "/KombatSim/static/js/19.368550cd.chunk.js"
+  },
+  {
+    "revision": "ffa158fda8cd156c18b4",
+    "url": "/KombatSim/static/js/2.de461af7.chunk.js"
+  },
+  {
+    "revision": "7d0b415f11f031c9ec9f",
+    "url": "/KombatSim/static/js/20.e6d394bb.chunk.js"
+  },
+  {
+    "revision": "725d8a881dba308c3f57",
+    "url": "/KombatSim/static/js/21.6b254e35.chunk.js"
+  },
+  {
+    "revision": "a7a6855b601756763129",
+    "url": "/KombatSim/static/js/22.aef81854.chunk.js"
+  },
+  {
+    "revision": "3a7861d07a4232270b0f",
+    "url": "/KombatSim/static/js/23.fd164af3.chunk.js"
+  },
+  {
+    "revision": "a2f75f195be798537b1d",
+    "url": "/KombatSim/static/js/24.71fd7088.chunk.js"
+  },
+  {
+    "revision": "04fa7b782cda051c422b",
+    "url": "/KombatSim/static/js/25.55330f85.chunk.js"
+  },
+  {
+    "revision": "1b375ac2d03b168e2bed",
+    "url": "/KombatSim/static/js/26.b1e8dd58.chunk.js"
+  },
+  {
+    "revision": "c5aa0f68f684ddab6b5b",
+    "url": "/KombatSim/static/js/27.d81ed6b2.chunk.js"
+  },
+  {
+    "revision": "0f489165629d9915cf8e",
+    "url": "/KombatSim/static/js/28.43e049d3.chunk.js"
+  },
+  {
+    "revision": "1f447530fd8ac723028f",
+    "url": "/KombatSim/static/js/29.1525675b.chunk.js"
+  },
+  {
+    "revision": "8741718058f25ef778a2",
+    "url": "/KombatSim/static/js/3.881c5628.chunk.js"
+  },
+  {
+    "revision": "5d2ccb3b3fbc7c2e289e",
+    "url": "/KombatSim/static/js/4.b42d3e95.chunk.js"
+  },
+  {
+    "revision": "dd919a0f41ade560b771",
+    "url": "/KombatSim/static/js/5.6a6b7946.chunk.js"
+  },
+  {
+    "revision": "1d8ff3776c3d6aba5bd7",
+    "url": "/KombatSim/static/js/6.d757f178.chunk.js"
+  },
+  {
+    "revision": "019f4d5e3314cd0bf60e",
+    "url": "/KombatSim/static/js/7.fe897234.chunk.js"
+  },
+  {
+    "revision": "53cbd40f4317614589fe",
+    "url": "/KombatSim/static/js/8.a1cec315.chunk.js"
+  },
+  {
+    "revision": "dcccc81c69bba25f019f",
+    "url": "/KombatSim/static/js/9.d566da49.chunk.js"
+  },
+  {
+    "revision": "899427cf660178b41f0e",
+    "url": "/KombatSim/static/js/main.2f44b713.chunk.js"
+  },
+  {
+    "revision": "20e9fc459b28a6d804ab",
+    "url": "/KombatSim/static/js/runtime~main.5967dd98.js"
+  }
+]);
