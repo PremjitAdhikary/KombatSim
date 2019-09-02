f2895c6ff27df52076531fd4dd8da2b27161c7db
Premjit Adhikary <adhikary.premjit@gmail.com>
2019-09-02 07:14:51 +0530

    Removed commented code

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
 import {NavLink} from 'react-router-dom';
 import  {getRoute} from '../../const/Routes';
 
 const Header = () => {
   return (
     <div className="header">
-      {/* <strong><em><Link to={getRoute('home').path}>Kombat Sim</Link></em></strong> */}
       <strong><em>
             <NavLink 
               exact 
